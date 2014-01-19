ed9b21de38415ad5fd0a4cd33a836907e46863eb
BerryDaniel <dberry@georemedy.com>
2014-01-19 01:38:04 +0100

    comments in script

--- scripts/clean_geometry_osm.sql
+++ scripts/clean_geometry_osm.sql
@@ -1,10 +1,18 @@
+/*
+clean_geometry_osm.sql requires that you install the cleangeometry function - http://www.kappasys.ch/pgtools/cleangeometry/cleanGeometry.sql
+
+install example :
+psql osm_latest < cleanGeometry.sql
+
+*/
+
 -- Validated the bad geometries found only contained one point
 delete from lines where not ST_IsValid(wkb_geometry);
 
 -- Step one to clean the bad geometries
 update multipolygons set wkb_geometry=ST_CollectionExtract(ST_Force_Collection(ST_MakeValid(wkb_geometry)),3);
 
--- Step two to clean the bad geometries (requires cleangeometry function - http://www.kappasys.ch/pgtools/cleangeometry/cleanGeometry.sql
+-- Step two to clean the bad geometries
 update multipolygons set wkb_geometry=cleangeometry(wkb_geometry);
 
 -- Final step, delete any remaining bad geometries
